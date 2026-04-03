da1cd9e0024507440db0696b8c6481f7dd21c9d0
wldbs31 <jiyunchoi31@gmail.com>
2026-04-03 16:06:24 -0400

    fix duplicate export default

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,4 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-
-export default function App() {
-  return (
-    <LangProvider>
-      <HashRouter> 
-        <Routes>
-          <Route path="/" element={<Envelope />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </HashRouter>
-    </LangProvider>
-  );
-}
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { LangProvider } from "./context/LangContext";
 import Envelope from "./pages/Envelope";
 import Home from "./pages/Home";
@@ -20,13 +6,13 @@ import Home from "./pages/Home";
 export default function App() {
   return (
     <LangProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Envelope />} />
           <Route path="/home" element={<Home />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </LangProvider>
   );
 }
