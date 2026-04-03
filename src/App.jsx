0bd40b30cdc98ae2489a5d5056f85f13af38732c
wldbs31 <jiyunchoi31@gmail.com>
2026-04-03 16:05:09 -0400

    fix base path and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+
+export default function App() {
+  return (
+    <LangProvider>
+      <HashRouter> 
+        <Routes>
+          <Route path="/" element={<Envelope />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </HashRouter>
+    </LangProvider>
+  );
+}
 import { LangProvider } from "./context/LangContext";
 import Envelope from "./pages/Envelope";
 import Home from "./pages/Home";
