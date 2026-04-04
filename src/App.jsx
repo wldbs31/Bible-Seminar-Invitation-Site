b0ec55dd63bfc596135b667223b5b24096da9e09
wldbs31 <jiyunchoi31@gmail.com>
2026-04-04 14:24:16 -0400

    fix base path

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { LangProvider } from "./context/LangContext";
 import Envelope from "./pages/Envelope";
 import Home from "./pages/Home";
